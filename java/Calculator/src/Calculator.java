2e101fc7742ad4dc93899ddada73794bcc0257d6
James <jh253@hw.ac.uk>
2018-08-13 02:39:30 +0100

    updated Calculator

--- java/Calculator/src/Calculator.java
+++ java/Calculator/src/Calculator.java
@@ -2,8 +2,31 @@
 
 public class Calculator {
 
+	private CalculatorController ctrl;
+	private CalculatorView view;
+	private CalculatorModel model;
+	
+	
+	public Calculator() {
+		view = new CalculatorView();
+		model = new CalculatorModel();
+		ctrl = new CalculatorController(view, model);
+		
+	}
+	
+	
 	public static void main(String[] args) {
-		System.out.println("test");
+		//new Calculator();
+		CalculatorModel model = new CalculatorModel();
+		//Testing add
+		System.out.println("2+3 = " + model.compute("2+3"));
+		//Testing mult
+		System.out.println("3*2 = " + model.compute("3*2"));
+		//Testing div
+		System.out.println("8/2 = " + model.compute("8/2"));
+		//Testing sub
+		System.out.println("7-3 = " + model.compute("7-3"));
+		
 	}
 
 }
