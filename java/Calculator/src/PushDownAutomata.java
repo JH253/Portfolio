858f75039fe6a0e5acacfed8e675781a9c89e9dc
James <jh253@hw.ac.uk>
2018-08-14 22:41:05 +0100

    created transition objects and state objects, the building blocks of the graph

--- java/Calculator/src/PushDownAutomata.java
+++ java/Calculator/src/PushDownAutomata.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class PushDownAutomata {
 	
+	private class Transition {
+		private State nextState;
+		private Character gateChar;
+		private Character gateStack;
+ 	}
+	
+	
 	private class State {
+		private ArrayList<Transition> transitions;
+		private String name;
 		
+		public State(String name, ArrayList<Transition> transitions) {
+			this.name = name;
+			this.transitions = transitions;
+		}
 	}
 	
 	private String tape;
@@ -11,9 +25,19 @@ public class PushDownAutomata {
 	private Stack<Character> stack;
 	
 	public PushDownAutomata() {
-		
+		this.stack = new Stack<>();
+		//init the graph for the pda
+		initGraph();
 	}
 
+	private void initGraph() {
+		ArrayList<Transition> aOutGoing = new ArrayList<>();
+		//TODO: Create outgoing transitions for init state
+		initState = new State("A", aOutGoing);
+		//TODO: Finish the rest of the graph
+		
+	}
+	
 	public Calculation parseCalculation(String input) {
 		return null;
 	}
